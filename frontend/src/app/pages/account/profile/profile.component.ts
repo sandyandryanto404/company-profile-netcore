44bb5d4621d6777bdbf19eeb5c408b28faadbe25
Sandy Andryanto <sandyandryanto@gmail.com>
2024-05-26 17:17:44 +0700

    up

--- frontend/src/app/pages/account/profile/profile.component.ts
+++ frontend/src/app/pages/account/profile/profile.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Title } from "@angular/platform-browser";
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { TooltipDirective } from '@babybeet/angular-tooltip';
+import { CommonModule } from '@angular/common';
+import { NgForm, FormsModule } from "@angular/forms"
+import { AccountService } from './../../../services/account.service';
+import { StorageService } from '../../../services/storage.service';
 import country from 'country-list-js';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FormsModule, CommonModule, TooltipDirective],
+  imports: [RouterModule, TooltipDirective, CommonModule, FormsModule],
   templateUrl: './profile.component.html',
   styles: ``
 })
 export class ProfileComponent implements OnInit {
 
-  title = environment.title;
-  auth:boolean = false;
+  auth:boolean = false
   loading:boolean = true;
+  title = environment.title;
+  loadingSubmit:boolean = false;
+  failed:boolean = false;
+  messageFailed:string = ""
+  messageSuccess:string = ""
   countries:any
   image = 'https://dummyimage.com/150x150/343a40/6c757d'
 
@@ -27,19 +34,23 @@ export class ProfileComponent implements OnInit {
      country: ""
   };
 
-  constructor(private titleService:Title, private router: Router) {
+  constructor(private accountService: AccountService, private titleService:Title, private router: Router, private storageService: StorageService) {
     this.titleService.setTitle("Manage Profile | " + this.title);
   }
 
-  ngOnInit(): void {
-      setTimeout(() => {
-        if(this.auth){
-          this.countries = country.names().sort();
-          this.loading = false
-        }else{
-          this.router.navigate(['/']);
-        }
+  ngOnInit(){
+    setTimeout(() => {
+       this.auth = this.storageService.isLoggedIn()
+       if(!this.auth){
+         this.router.navigate(['/auth/login']);
+       }else{
+          this.loadContent()
+       }
     }, 2000)
-  }
+ }
+
+ loadContent(): void{
+
+ }
 
 }
