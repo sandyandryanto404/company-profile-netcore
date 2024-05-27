1029bcab32ac9a92dc4b450b016449622eb77d7a
Sandy Andryanto <sandyandryanto@gmail.com>
2024-05-27 08:56:21 +0700

    up

--- frontend/src/app/pages/article/list/list.component.ts
+++ frontend/src/app/pages/article/list/list.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Title } from "@angular/platform-browser";
-
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ArticleService } from '../../../services/article.service';
+import { PageService } from '../../../services/page.service';
+import moment from "moment"
 @Component({
   selector: 'app-list-article',
   standalone: true,
@@ -13,15 +18,37 @@ export class ListComponent implements OnInit {
 
   title = environment.title;
   loading:boolean = true;
+  message:string = ""
+  failed:boolean = false
+  content:any
 
-  constructor(private titleService:Title) {
+  constructor(private articleService: ArticleService, private pageService: PageService, private titleService:Title, private router: Router) {
     this.titleService.setTitle("Article | " + this.title);
   }
 
+
   ngOnInit(): void {
-      setTimeout(() => {
-        this.loading = false
-    }, 3000)
+    this.pageService.ping().subscribe((response: any) => {
+        this.loadContent()
+    }, (error) => {
+        console.log(error)
+        this.router.navigate(['/unavailable']);
+    });
+  }
+
+  loadContent(): void{
+    var params:any = {}
+    var queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+    this.articleService.list(queryString).subscribe((response: any) => {
+        setTimeout(() => {
+            console.log(response.data)
+            this.content = response.data;
+            this.loading = false;
+        }, 1500)
+    }, (error) => {
+        console.log(error)
+        this.router.navigate(['/unavailable']);
+    });
   }
 
 }
